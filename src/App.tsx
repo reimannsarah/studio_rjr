5ebd510edc18d6b90e3a1cc0080386e83355b53e
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-24 14:33:35 -0800

    add useEffect to load all site images into session storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,27 @@
-import './App.css'
-import Hero from './components/main/Hero'
+import { useEffect } from "react";
+import "./App.css";
+import Hero from "./components/main/Hero";
+import main from "./services/blobServiceClient";
 
 function App() {
+  useEffect(() => {
+    main()
+      .then((blobInfos) => {
+        sessionStorage.setItem("blobInfos", JSON.stringify(blobInfos));
+      })
+      .catch((err) => console.error(err));
+
+    const storedBlobInfos = sessionStorage.getItem("blobInfos");
+    if (storedBlobInfos) {
+      console.log(JSON.parse(storedBlobInfos));
+    }
+  }, []);
 
   return (
     <>
       <Hero />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
