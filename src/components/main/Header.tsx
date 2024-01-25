be45a43be0a6a1201ab17b295482268aba3ffcb7
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-25 15:06:49 -0800

    animate header

--- src/components/main/Header.tsx
+++ src/components/main/Header.tsx
@@ -1,7 +1,18 @@
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../utils/motion";
+import { NavLink } from "react-router-dom";
+
 export const Header = () => {
   return (
-    <div className="fixed h-16 w-16 lg:inset-y-96 left-0 lg:w-16 z-600 lg:mt-[200px] ml-8 z-600 sm:text-center">
-      <h1 className="text-primary lg:text-[100px] font-header lg:transform lg:-rotate-90">StudioRJR</h1>
-    </div>
+    <motion.div
+      variants={fadeIn(4)}
+      initial="hidden"
+      animate="visible"
+      className="absolute h-16 w-16 left-0 lg:w-16 z-600  ml-8 z-600 sm:text-center"
+    >
+      <NavLink to="/">
+        <h1 className="text-primary lg:text-[100px] font-header">StudioRJR</h1>
+      </NavLink>
+    </motion.div>
   );
 };
