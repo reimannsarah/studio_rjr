71b0c0cfe6813c2a2b4582654a94b28a6b071b0c
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-25 14:26:32 -0800

    add header and footer to layout

--- src/components/main/Layout.tsx
+++ src/components/main/Layout.tsx
@@ -1,13 +1,17 @@
 import { Outlet } from 'react-router-dom';
 import Navbar from './NavBar'; 
+import { Header } from './Header';
+import { Footer } from './Footer';
 
 const Layout = () => {
   return (
     <div className="flex flex-col min-h-screen">
+      <Header />
       <Navbar />
       <div className="flex-grow">
         <Outlet />
       </div>
+      <Footer />
     </div>
   );
 };
