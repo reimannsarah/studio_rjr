e122955cb92bb8874bff1d4971abad5f7ebe3c07
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-25 15:06:35 -0800

    animate footer

--- src/components/main/Footer.tsx
+++ src/components/main/Footer.tsx
@@ -1,9 +1,16 @@
-import { pageStrings } from '../../assets/text/pageStrings'
+import { pageStrings } from "../../assets/text/pageStrings";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../utils/motion";
 
 export const Footer = () => {
   return (
-    <footer className='text-primary flex justify-end mb-[10px]'>
-      <p>{pageStrings.copyright}</p>
-    </footer>
-  )
-}
+    <motion.footer
+      variants={fadeIn(6)}
+      initial="hidden"
+      animate="visible"
+      className="text-primary fixed bottom-0 left-0 h-16 w-16"
+    >
+      <p className="transform -rotate-90">{pageStrings.copyright}</p>
+    </motion.footer>
+  );
+};
