8f58d7ebe23216476453bad409a8e479448346f9
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-25 14:26:16 -0800

    add header component

---
+++ src/components/main/Header.tsx
@@ -0,0 +1,7 @@
+export const Header = () => {
+  return (
+    <div className="fixed h-16 w-16 lg:inset-y-96 left-0 lg:w-16 z-600 lg:mt-[200px] ml-8 z-600 sm:text-center">
+      <h1 className="text-primary lg:text-[100px] font-header lg:transform lg:-rotate-90">StudioRJR</h1>
+    </div>
+  );
+};
