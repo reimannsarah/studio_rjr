905c46fe224fe19ebd1a751193e013ad256b3dc2
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-25 11:29:46 -0800

    update App

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
+import './App.css';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import "./App.css";
 import Hero from "./components/main/Hero";
-import MixedMedia from "./components/main/MixedMedia";
-import AbstractPaintings from "./components/main/AbstractPaintings";
-import Portraits from "./components/main/Portraits";
-import CharcoalDrawings from "./components/main/CharcoalDrawings";
-import NavBar from './components/main/NavBar';
+import Layout from './components/main/Layout';
+import Art from './components/main/Art';
 
 function App() {
   return (
     <Router>
-      <NavBar />
       <Routes>
-        <Route path="/mixed-media" element={<MixedMedia />} />
-        <Route path="/abstract-paintings" element={<AbstractPaintings />} />
-        <Route path="/portraits" element={<Portraits />} />
-        <Route path="/charcoal-drawings" element={<CharcoalDrawings />} />
-        <Route path="/" element={<Hero />} />
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Hero />} />
+          <Route path="/:route" element={<Art />} />
+        </Route>
       </Routes>
     </Router>
   );
