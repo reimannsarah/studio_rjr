332978d4430fc837e37c413f2246d4fe49187a21
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-25 13:18:02 -0800

    add loading bar

--- src/components/main/Art.tsx
+++ src/components/main/Art.tsx
@@ -1,20 +1,51 @@
-import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import main, { BlobInfo } from '../../services/blobServiceClient'; 
+import { useEffect, useState } from "react";
+import { useParams, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+import { fadeIn } from "../../../utils/motion";
+import main, { BlobInfo } from "../../services/blobServiceClient";
 
-const Art = () => {
+const ArtData = () => {
   const { route } = useParams<{ route: string }>();
   const [blobs, setBlobs] = useState<BlobInfo[]>([]);
   const [isLoading, setIsLoading] = useState(true);
+  const [progress, setProgress] = useState(0);
+  const [key, setKey] = useState(Math.random());
+  const location = useLocation();
 
+  useEffect(() => {
+    setIsLoading(true);
+  }, [location]);
+
+  useEffect(() => {
+    if (isLoading) {
+      const timer = setInterval(() => {
+        setProgress((oldProgress) => {
+          if (oldProgress >= 100) {
+            clearInterval(timer);
+            return 100;
+          }
+          return oldProgress + 10;
+        });
+      }, 200);
+      return () => clearInterval(timer);
+    } else {
+      setProgress(0);
+    }
+  }, [isLoading]);
+
+  console.log(progress);
   useEffect(() => {
     main()
       .then((blobInfos) => {
-        const filteredBlobs = blobInfos.filter((blobInfo) => blobInfo.metadata && blobInfo.metadata[route as keyof typeof blobInfo.metadata] === 'true');
+        const filteredBlobs = blobInfos.filter(
+          (blobInfo) =>
+            blobInfo.metadata &&
+            blobInfo.metadata[route as keyof typeof blobInfo.metadata] ===
+              "true"
+        );
         setBlobs(filteredBlobs);
         setIsLoading(false);
-        console.log(route)
-        console.log(filteredBlobs)
+        setKey(Math.random());
       })
       .catch((err) => {
         console.error(err);
@@ -23,16 +54,37 @@ const Art = () => {
   }, [route]);
 
   if (isLoading) {
-    return <div>Loading...</div>; // Replace this with your loading graphic
+    return (
+      <div className="w-full h-[30px] rounded-lg border-4 border-black mt-[400px]">
+        <div
+          style={{ width: `${progress}%` }}
+          className="h-full rounded-sm bg-[#21ACAB] transition-width duration-500 ease-out"
+        ></div>
+      </div>
+    );
   }
 
   return (
-    <div className='flex flex-row flex-wrap gap-10 justify-center my-[100px]'>
+    <motion.div
+      className="flex flex-row flex-wrap gap-10 justify-center my-[100px]"
+      key={key}
+    >
       {blobs.map((blobInfo, index) => (
-        <img className="object-contain" key={index} src={sessionStorage.getItem(blobInfo.name) || ""} alt={blobInfo.name} />
+        <motion.div
+          variants={fadeIn(0.5)}
+          initial="hidden"
+          animate="visible"
+          key={index}
+        >
+          <img
+            className="object-contain"
+            src={sessionStorage.getItem(blobInfo.name) || ""}
+            alt={blobInfo.name}
+          />
+        </motion.div>
       ))}
-    </div>
+    </motion.div>
   );
-}
+};
 
-export default Art;
+export default ArtData;
