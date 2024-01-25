2e75b97f68fd5838c6791d35683874806f7cc115
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-25 11:30:33 -0800

    update navbar routes

--- src/components/main/NavBar.tsx
+++ src/components/main/NavBar.tsx
@@ -2,20 +2,20 @@ import { NavLink } from "react-router-dom";
 
 const NavBar = () => {
   return (
-    <div className="fixed top-0 right-0 space-y-4 p-4">
+      <div className="fixed right-0 h-16 w-50 p-5 gap-5 flex flex-row z-600">
         <NavLink to="/">
           <p>Home</p>
         </NavLink>
-        <NavLink to="/mixed-media">
+        <NavLink to="/mixed">
           <p>Mixed Media</p>
         </NavLink>
-        <NavLink to="/abstract-paintings">
+        <NavLink to="/abstract">
           <p>Abstract Paintings</p>
         </NavLink>
-        <NavLink to="/portraits">
+        <NavLink to="/portrait">
           <p>Portraits</p>
         </NavLink>
-        <NavLink to="/charcoal-drawings">
+        <NavLink to="/charcoal">
           <p>Charcoal Drawings</p>
         </NavLink>
       </div>
