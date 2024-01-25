e7b77892ddfee3923fb335c5b259e761cabcdce2
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-25 15:07:31 -0800

    animate and style hover

--- src/components/main/NavBar.tsx
+++ src/components/main/NavBar.tsx
@@ -1,24 +1,48 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
+import { motion } from "framer-motion";
+import { slideInFromTop } from "../../../utils/motion";
 
 const NavBar = () => {
+  const location = useLocation();
+
   return (
-      <div className="fixed right-0 h-16 w-50 p-5 gap-5 flex flex-row z-600 text-primary">
-        <NavLink to="/">
-          <p>Home</p>
-        </NavLink>
-        <NavLink to="/mixed">
-          <p>Mixed Media</p>
-        </NavLink>
-        <NavLink to="/abstract">
-          <p>Abstract Paintings</p>
-        </NavLink>
-        <NavLink to="/portrait">
-          <p>Portraits</p>
-        </NavLink>
-        <NavLink to="/charcoal">
-          <p>Charcoal Drawings</p>
-        </NavLink>
-      </div>
+    <motion.div
+      variants={slideInFromTop(5)}
+      initial="hidden"
+      animate="visible"
+      className="absolute right-10 h-16 w-50 p-5 gap-5 flex flex-row z-600 text-primary mt-[50px]"
+    >
+      <NavLink
+        to="/"
+        className={location.pathname === "/" ? "text-white" : "text-primary"}
+      >
+        <p className="hover:line-through">Home</p>
+      </NavLink>
+      <NavLink
+        to="/mixed"
+        className={location.pathname === "/mixed" ? "text-white" : "text-primary"}
+      >
+        <p className="hover:line-through">Mixed Media</p>
+      </NavLink>
+      <NavLink
+        to="/abstract"
+        className={location.pathname === "/abstract" ? "text-white" : "text-primary"}
+      >
+        <p className="hover:line-through">Abstract Paintings</p>
+      </NavLink>
+      <NavLink
+        to="/portrait"
+        className={location.pathname === "/portrait" ? "text-white" : "text-primary"}
+      >
+        <p className="hover:line-through">Portraits</p>
+      </NavLink>
+      <NavLink
+        to="/charcoal"
+        className={location.pathname === "/charcoal" ? "text-white" : "text-primary"}
+      >
+        <p className="hover:line-through">Charcoal Drawings</p>
+      </NavLink>
+    </motion.div>
   );
 };
 
