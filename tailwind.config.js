88f1caede18c9a9d32255eae81ef1b6ffde592e9
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-25 13:59:33 -0800

    add fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{html,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{html,js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'header': ["Archivo Black", "sans-serif"],
+        'subheader': ["Barlow", "sans-serif"],
+      },
+      colors: {
+        'primary': '#5f6265'
+      }
+    },
+    plugins: [],
   },
-  plugins: [],
-}
-
+};
