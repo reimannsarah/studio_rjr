5af3bcad4cdc9cdfd1717967eb7b22d7173d23bc
Sarah Reimann <reimannsarah45@gmail.com>
2024-01-24 21:19:01 -0800

    add hero component

--- src/components/main/Hero.tsx
+++ src/components/main/Hero.tsx
@@ -1,8 +1,25 @@
+import { useEffect, useState } from 'react';
+import main, { BlobInfo } from '../../services/blobServiceClient'; 
 
 const Hero = () => {
+  const [heroBlobs, setHeroBlobs] = useState<BlobInfo[]>([]);
+
+  useEffect(() => {
+    main()
+      .then((blobInfos) => {
+        const heroBlobs = blobInfos.filter((blobInfo) => blobInfo.metadata && blobInfo.metadata.hero === 'true');
+        setHeroBlobs(heroBlobs);
+      })
+      .catch((err) => console.error(err));
+  }, []);
+
   return (
-    <div>Hero</div>
-  )
+    <div>
+      {heroBlobs.map((blobInfo, index) => (
+        <img key={index} src={sessionStorage.getItem(blobInfo.name) || ""} alt={blobInfo.name} />
+      ))}
+    </div>
+  );
 }
 
-export default Hero
+export default Hero;
